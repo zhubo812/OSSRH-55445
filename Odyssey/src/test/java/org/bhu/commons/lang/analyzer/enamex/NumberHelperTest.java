eaef0a8d035898dafddc5f3748b8e113a6148c3c
Jackie <zhubo812@gmail.com>
2022-01-13 20:27:31 +0800

    update

--- Odyssey/src/test/java/org/bhu/commons/lang/analyzer/enamex/NumberHelperTest.java
+++ Odyssey/src/test/java/org/bhu/commons/lang/analyzer/enamex/NumberHelperTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 
 public class NumberHelperTest {
 
-	@Test
-	public void recognitionTester() {
-		NumberHelper nh = new NumberHelper();
-		String line = "丰田这招实在高考验中国人民抵制日货的机会来了395";
-		List<Entity> list =  nh.getNumber(line);
-		for(Entity e : list) {
-			System.out.println(e.getExpression());
-		}
-	}
+//	@Test
+//	public void recognitionTester() {
+//		NumberHelper nh = new NumberHelper();
+//		String line = "丰田这招实在高考验中国人民抵制日货的机会来了395";
+//		List<Entity> list =  nh.getNumber(line);
+//		for(Entity e : list) {
+//			System.out.println(e.getExpression());
+//		}
+//	}
 }
