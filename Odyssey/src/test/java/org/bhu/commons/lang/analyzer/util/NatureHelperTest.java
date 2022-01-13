76304c1452699d85aee3103117f94eac3b9c4ed4
Jackie <zhubo812@gmail.com>
2022-01-13 20:21:16 +0800

    update

--- Odyssey/src/test/java/org/bhu/commons/lang/analyzer/util/NatureHelperTest.java
+++ Odyssey/src/test/java/org/bhu/commons/lang/analyzer/util/NatureHelperTest.java
@@ -7,36 +7,36 @@ import org.junit.Test;
 
 public class NatureHelperTest {
 
-	
-	@Test
-	public void tester() {
-		List<String> list = new ArrayList<String>();
-		for(int i=0;i< 10; i++) {
-			list.add(String.valueOf(i));
-		}
-		
-		fixedGrouping(list, 3);
-	}
-	
-	
-	public static <T> List<List<T>> fixedGrouping(List<T> source, int n) {
-
-	    if (null == source || source.size() == 0 || n <= 0)
-	        return null;
-	    List<List<T>> result = new ArrayList<List<T>>();
-	    int remainder= 0;
-	    for (int i = 0; i < source.size()-2; i++) {
-	        List<T> subset = null;
-	        subset = source.subList(i * (n-1), i * (n-1)+n);
-	        System.out.println(subset);
-	        result.add(subset);
-	        remainder = source.size() -(i * (n-1)+n);
-	    }
-	    if (remainder > 0) {
-	        List<T> subset = null;
-	        subset = source.subList(remainder,source.size()-1);
-	        result.add(subset);
-	    }
-	    return result;
-	}
+//	
+//	@Test
+//	public void tester() {
+//		List<String> list = new ArrayList<String>();
+//		for(int i=0;i< 10; i++) {
+//			list.add(String.valueOf(i));
+//		}
+//		
+//		fixedGrouping(list, 3);
+//	}
+//	
+//	
+//	public static <T> List<List<T>> fixedGrouping(List<T> source, int n) {
+//
+//	    if (null == source || source.size() == 0 || n <= 0)
+//	        return null;
+//	    List<List<T>> result = new ArrayList<List<T>>();
+//	    int remainder= 0;
+//	    for (int i = 0; i < source.size()-2; i++) {
+//	        List<T> subset = null;
+//	        subset = source.subList(i * (n-1), i * (n-1)+n);
+//	        System.out.println(subset);
+//	        result.add(subset);
+//	        remainder = source.size() -(i * (n-1)+n);
+//	    }
+//	    if (remainder > 0) {
+//	        List<T> subset = null;
+//	        subset = source.subList(remainder,source.size()-1);
+//	        result.add(subset);
+//	    }
+//	    return result;
+//	}
 }
